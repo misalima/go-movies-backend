a8665fc46152eaa946853b6c3108843b1c1a0b01
Misael Lima <misael.alisson14@gmail.com>
2023-08-27 14:26:00 -0300

    add delete functionality

--- internal/repository/dbrepo/postgres_dbrepo.go
+++ internal/repository/dbrepo/postgres_dbrepo.go
@@ -320,6 +320,39 @@ func (m *PostgresDBRepo) InsertMovie(movie models.Movie) (int, error) {
 
 }
 
+func (m *PostgresDBRepo) UpdateMovie(movie models.Movie) error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	stmt := `update movies set
+				title = $1,
+				description = $2,
+				release_date = $3,
+				runtime = $4,
+				mpaa_rating = $5,
+				updated_at = $6,
+				image = $7
+				where id = $8`
+
+	_, err := m.DB.ExecContext(ctx, stmt,
+		movie.Title,
+		movie.Description,
+		movie.ReleaseDate,
+		movie.RunTime,
+		movie.MPAARating,
+		movie.UpdatedAt,
+		movie.Image,
+		movie.ID,
+	)
+
+	if err != nil {
+		return err
+	} 
+
+	return nil
+	
+}
+
 func (m *PostgresDBRepo) UpdateMovieGenres(id int, genreIDs []int) error {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
@@ -338,5 +371,19 @@ func (m *PostgresDBRepo) UpdateMovieGenres(id int, genreIDs []int) error {
 			return err
 		}
 	}
+	return nil
+}
+
+func (m *PostgresDBRepo) DeleteMovie(id int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	stmt := `delete from movies where id = $1`
+
+	_, err := m.DB.ExecContext(ctx, stmt, id)
+	if err != nil {
+		return err
+	} 
+
 	return nil
 }
